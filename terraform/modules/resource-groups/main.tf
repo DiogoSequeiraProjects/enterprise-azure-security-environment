6d47fe27ad11493d40f32e045ea9cb51128fa7df
DiogoSequeiraProjects <diogo.sequeira2004@gmail.com>
2026-06-25 22:43:28 +0100

    Update main.tf

--- terraform/modules/resource-groups/main.tf
+++ terraform/modules/resource-groups/main.tf
@@ -1,1 +1,36 @@
 
+resource "azurerm_resource_group" "network" {
+  name     = "rg-enterprise-network"
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_resource_group" "security" {
+  name     = "rg-enterprise-security"
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_resource_group" "monitoring" {
+  name     = "rg-enterprise-monitoring"
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_resource_group" "identity" {
+  name     = "rg-enterprise-identity"
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_resource_group" "dev" {
+  name     = "rg-enterprise-dev"
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_resource_group" "prod" {
+  name     = "rg-enterprise-prod"
+  location = var.location
+  tags     = var.tags
+}
